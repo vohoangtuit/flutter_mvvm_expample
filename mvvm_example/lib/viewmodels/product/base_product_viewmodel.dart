0b2efd7ac836040df35006df71a4469724a061a5
vohoangtuit <vohoangtu2401@gmail.com>
2020-08-31 09:25:18 +0700

    update code

--- mvvm_example/lib/viewmodels/product/base_product_viewmodel.dart
+++ mvvm_example/lib/viewmodels/product/base_product_viewmodel.dart
@@ -2,7 +2,7 @@ import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
 import 'package:mvvm/mvvm.dart';
 import 'package:mvvm_example/base/bases_statefulwidget.dart';
-import 'file:///C:/TU/Develop/Demo/flutter_mvvm_example/mvvm_example/lib/network/product/product_server.dart';
+import 'package:mvvm_example/network/product/product_server.dart';
 
  class ProductBaseViewModel extends ViewModel{
   Dio dio;
